5111b77d99491222e25fadff12a306ee86f4d8fb
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-07-29 08:09:02 -0300

    ♻️ refactor(checkIsUserAuthenticated): added check on token value

--- src/utils/checkIsUserAuthenticated.ts
+++ src/utils/checkIsUserAuthenticated.ts
@@ -1,8 +1,17 @@
 import { verify } from 'jsonwebtoken';
-import { ResponseProps } from '../types';
+
 import { throwError } from './throwError';
 
+import { ResponseProps } from '../types';
+
 export const checkIsUserAuthenticated = (token: string, res: ResponseProps) => {
+  if (token === undefined) {
+    return throwError({
+      res,
+      message: 'You must be authenticated to access this route.',
+    });
+  }
+
   try {
     const authToken = token.replace('Bearer ', '');
     const isValidToken = verify(authToken, process.env.JWT_SECRET_KEY!);
